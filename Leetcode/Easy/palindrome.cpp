36610962dd5239a7cc2dbca50863f036080bb1bc
Sayan Basu <sayanbasu@Sayans-MacBook-Air-2.local>
2025-08-01 23:28:58 +0530

    Added

---
+++ Leetcode/Easy/palindrome.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s[i] != s[s.length() - 1 - i]) {
+                return false;  
+            }
+        }
+        return true;  
+    }
+};
